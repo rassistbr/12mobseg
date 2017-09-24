99b5cf31e9dd7d16b6606a93a6c184b5cb95bb80
rassistbr <rtorresbr@live.copm>
2017-09-24 17:07:09 -0300

    Add project files.

---
+++ 12mob/Default.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="_12mob._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="jumbotron">
+        <p class="lead">Usuário:</p>
+        <asp:TextBox ID="TextBoxusername" runat="server"></asp:TextBox>
+        <br>
+        <p class="lead">Senha: </p><asp:TextBox ID="TextBoxpassword" runat="server"></asp:TextBox>
+        <asp:Button ID="Buttonlogin" class="btn btn-primary btn-lg" runat="server" onclick="Buttonlogin_Click" Text="Login" />
+        <br>
+    </div>
+    <div class="jumbotron">
+        <p class="lead"><asp:Label ID="Labelinfo" runat="server" Text=""></asp:Label></p>
+    </div>
+</asp:Content>
---
+++ 12mob/Default.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _12mob
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Buttonlogin_Click(object sender, EventArgs e)
+        {
+            string strcon = ConfigurationManager.ConnectionStrings["azuresqldb"].ConnectionString;
+            SqlConnection con = new SqlConnection(strcon);
+
+            SqlCommand com = new SqlCommand("SELECT * FROM registrationtab WHERE username = '"
+                                            + TextBoxusername.Text
+                                            + "' AND password = '"
+                                            + TextBoxpassword.Text + "'", con);
+
+            //SqlCommand com = new SqlCommand("SELECT * FROM registrationtab WHERE username = @user AND password = @password", con);
+            //com.Parameters.AddWithValue("@user", TextBoxusername.Text);
+            //com.Parameters.AddWithValue("@password", TextBoxpassword.Text);
+
+            con.Open();
+            SqlDataReader rd = com.ExecuteReader();
+            if (rd.HasRows)
+            {
+                rd.Read();
+                Labelinfo.Text = "Login successful.";
+            }
+
+            else
+            {
+                Labelinfo.Text = "Invalid username or password.";
+
+            }
+        }
+    }
+}
